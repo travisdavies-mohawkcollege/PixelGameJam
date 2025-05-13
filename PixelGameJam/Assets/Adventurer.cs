8cdd5a9abda53a8e34773b13761e720ae0be7dae
Ephi Robinson <ephirobinson@gmail.com>
2025-05-13 11:41:04 -0400

    add
     cork

---
+++ PixelGameJam/Assets/Adventurer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adventurer : MonoBehaviour
+{
+    public string names[] names = new "
+        ;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
