5efb4651cf95a21d1b4b018264cc6a58ce60a86b
travisdavies-mohawkcollege <travis.davies@mohawkcollege.ca>
2025-05-13 01:43:14 -0400

    Start dungeon manager

--- PixelGameJam/Assets/Scripts/DungeonManager.cs
+++ PixelGameJam/Assets/Scripts/DungeonManager.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class DungeonManager : MonoBehaviour
 {
+    //Declare Variables
+    public GameObject dungeonPrefab;
+    public GameObject hoardPrefab;
+    public GameObject playerRoomPrefab;
+    public GameObject nextRoomMarker;
+    int roomCount;
+    int roomsOwned;
+    float roomWidth;
+    float roomHeight;
+    bool isLocked;
+    bool hasTrap;
+    float roomCentre;
+    float roomOffsetX;
+    List<GameObject> rooms = new List<GameObject>();
+
+
     // Start is called before the first frame update
     void Start()
     {
+        roomCount = 0;
+        roomsOwned = 3;
+        roomOffsetX = 0;
+        roomWidth = dungeonPrefab.transform.localScale.x;
+        roomHeight = dungeonPrefab.transform.localScale.y;
+        roomCentre = roomWidth / 2;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (roomCount <= roomsOwned)
+        {
+            CreateRoom();
+        }
+    }
+
+    void CreateRoom()
+    {
+        //Create a new room
+        roomCount += 1;
+        roomOffsetX -= roomWidth;
         
+        GameObject newRoom = Instantiate(dungeonPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        rooms.Add(newRoom);
+
+        newRoom.transform.position = new Vector3(roomOffsetX - roomCentre, 0, 0);
     }
 }
